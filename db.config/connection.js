e4e57424e003737039ac9c524fb16779998a12a2
Hariks17 <hariksh.94@gmail.com>
2021-03-10 12:58:22 +0530

    Added Mongoose

--- db.config/connection.js
+++ db.config/connection.js
@@ -1,10 +1,11 @@
 const mongoose=require('mongoose');
 
+//****Add Your Cluster URI****//
 const URI= "";
 
 const connectDB = async ()=>{
     await mongoose.connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('db connected...!');
+    console.log('db connected...!');//This will be printed in console if connection is successfull
 }
 
 module.exports=connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express=require('express');
+
+//****Importing the Configuration****//
 const connectDB= require('./db.config/connection')
 
+//****Setting PORT****//
 const PORT=process.env.PORT || 3000;
 
 const app=express();
-app.use(express.json({extended:false}))
-app.use('/api/userModel',require('./api/user'))
+//****Creating Connection****//
 connectDB();
 app.listen(PORT,()=>{
     console.log(`Server is running at port ${PORT}`);
